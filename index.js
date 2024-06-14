1f1f84658c28a38a201d650fb8d865c0e2336dd3
brianpocasangre <brianpocasangre1@gmail.com>
2024-06-14 14:07:31 -0700

    Add axios get function and route to index.ejs with random brewery on load

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+const API_URL = 'https://api.openbrewerydb.org/v1/breweries/random';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(API_URL);
+    const result = response.data[0];
+    console.log(result);
+    res.render('index.ejs', {
+      data: result,
+    });
+  } catch (error) {
+    console.log(error.data.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on Port:${port}, http://localhost:${port}`);
+});
